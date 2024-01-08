bf595a6d9b95dfc34fc1c5c4f465d7bb9e4e317f
srss-pocs <145287517+srss-pocs@users.noreply.github.com>
2024-01-08 23:45:46 +0530

    Configure creds in props file

---
+++ credentials/src/test/java/com/example/credentials/CredentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.credentials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
